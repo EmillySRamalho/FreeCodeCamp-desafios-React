43887b9dd8a712b439473f1e902632f2e136ed89
EmillySRamalho <emilly_bsfpb@hotmail.com>
2026-02-19 16:54:58 -0300

    criacao de footer

---
+++ Footer/index.jsx
@@ -0,0 +1,20 @@
+export function Footer(){
+  return (
+    <footer>
+    <ul>
+    <li><a href = '#'>Endereço</a></li>
+    <li>Telefone: 9 5414-9858</li>
+    </ul>
+     <ul>
+    <li> <a href = '#'>Instagram</a></li>
+    <li>Rede sociais</li>
+    </ul>
+     <ul>
+    <li><a href = '#'>Entre em conatato </a></li>
+    <li>Estamos sempre a disposição</li>
+    </ul>
+    <p>© todos os direitos reservados</p>
+
+    </footer>
+  )
+}
